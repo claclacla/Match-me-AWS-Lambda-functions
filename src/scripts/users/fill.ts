c0c586fb63f75195f424153c254440c20005a838
Admin <admin@MacBook-Pro-di-Admin.local>
2025-07-06 11:40:54 +0200

    The script fill has been rewritten using DynamoDB

--- src/scripts/users/fill.ts
+++ src/scripts/users/fill.ts
@@ -1,83 +1,63 @@
-/*
 import * as dotenv from 'dotenv';
 import { v4 as uuidv4 } from 'uuid';
-import { Pinecone } from '@pinecone-database/pinecone';
 
-import { DATA, PINECONE } from "../../config/config.json";
 import usersDataset from '../../../assets/users.json';
+
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 import { UserDTO } from '../../dtos/UserDTO';
 import { UserEntity } from '../../entities/UserEntity';
 
 import { connect as openAIConnect } from '../../openai/connect';
-import { generateTextEmbedding } from '../../openai/generateTextEmbedding';
-import { generateUserNarrative } from '../../openai/generateUserNarrative';
-import { generateIdealMatchProfile } from '../../openai/generateIdealMatchProfile';
+import { generateGroupBehavior } from '../../openai/generateGroupBehavior';
 
-import { connect as pineconeConnect } from '../../repositories/pinecone/connect';
-import { upsert as pineconeUsersUpsert } from "../../repositories/pinecone/users";
+import { connect as dynamoDBConnect } from '../../repositories/dynamoDB/connect';
+import { upsert as dynamoDBUpsert } from '../../repositories/dynamoDB/users';
 
 dotenv.config();
 
-const PINECONE_KEY = process.env.PINECONE_KEY;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
-if (PINECONE_KEY === undefined || OPENAI_API_KEY === undefined) {
+const AWS_REGION = process.env.AWS_REGION;
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+if (!OPENAI_API_KEY || !AWS_REGION || !AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+    console.error("Missing required environment variables.");
     process.exit(1);
 }
 
 const users: UserDTO[] = usersDataset as UserDTO[];
 
-const pineconeClient: Pinecone = pineconeConnect({ key: PINECONE_KEY });
-
+const dynamoDBClient: DynamoDBDocumentClient = dynamoDBConnect({ region: AWS_REGION, accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY });
 const openai = openAIConnect({ key: OPENAI_API_KEY });
 
 async function fill() {
     try {
-        console.log(`Creation of the index upsert "${PINECONE.INDEXES.USERS}"...`);
+        console.log(`Inserting the data set users...`);
 
         const usersEntities: UserEntity[] = [];
 
         for (const user of users) {
-            const narrative: string = await generateUserNarrative({ openai, insights: user.insights });
-            const narrativeEmbedding = await generateTextEmbedding({
-                openai,
-                text: narrative,
-                dimension: DATA.EMBEDDING_DIMENSION
-            });
-
-            const idealMatchProfile: string = await generateIdealMatchProfile({ openai, narrative });
-            const idealMatchProfileEmbedding = await generateTextEmbedding({
-                openai,
-                text: idealMatchProfile,
-                dimension: DATA.EMBEDDING_DIMENSION
-            });
+            const groupBehavior: string = await generateGroupBehavior({ openai, insights: user.insights });
 
             const userEntity: UserEntity = {
                 id: user.id,
-                values: narrativeEmbedding,
-                metadata: {
-                    ownerId: uuidv4(),
-                    name: user.name,
-                    gender: user.gender,
-                    location: user.location,
-                    age: user.age,
-                    insights: user.insights,
-                    narrative,
-                    idealMatchProfile,
-                    //idealMatchProfileEmbedding,
-                    matchId: ""
-                },
+                ownerId: uuidv4(),
+                name: user.name,
+                gender: user.gender,
+                location: user.location,
+                yearOfBirth: user.yearOfBirth,
+                insights: user.insights,
+                groupBehavior
             };
 
             usersEntities.push(userEntity);
         }
 
-        console.log(`${usersEntities.length} vectors creation in the index "${PINECONE.INDEXES.USERS}"...`);
+        await dynamoDBUpsert({ dynamoDBClient, users: usersEntities });
 
-        await pineconeUsersUpsert({ pineconeClient, users: usersEntities });
-
-        console.log("Vectors created!");
+        console.log("New users Added!");
 
     } catch (error: any) {
         console.error("Upsert error:", error.message || error);
@@ -85,4 +65,3 @@ async function fill() {
 }
 
 fill();
-*/
